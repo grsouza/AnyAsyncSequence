4f5097d8a2c8d10fd5e2699c5d8a94e0c8e3c7a3
Guilherme Souza <grsouza@pm.me>
2021-09-24 16:00:30 -0300

    Init package

---
+++ Sources/AnyAsyncSequence/AnyAsyncSequence.swift
@@ -0,0 +1,35 @@
+public struct AnyAsyncSequence<Element>: AsyncSequence {
+    public typealias AsyncIterator = AnyAsyncIterator<Element>
+
+    let _makeAsyncIterator: () -> AsyncIterator
+    
+    public init<S: AsyncSequence>(_ s: S) where S.Element == Element {
+        _makeAsyncIterator = {
+            AnyAsyncIterator(s.makeAsyncIterator())
+        }
+    }
+    
+    __consuming public func makeAsyncIterator() -> AsyncIterator {
+        _makeAsyncIterator()
+    }
+}
+
+public struct AnyAsyncIterator<Element>: AsyncIteratorProtocol {
+    let _next: () async throws -> Element?
+
+    public init<Iterator: AsyncIteratorProtocol>(_ iterator: Iterator) where Iterator.Element == Element {
+        var iterator = iterator
+        _next = { try await iterator.next() }
+    }
+    
+    public mutating func next() async throws -> Element? {
+        try await _next()
+    }
+    
+}
+
+extension AsyncSequence {
+    public func eraseToAnyAsyncSequence() -> AnyAsyncSequence<Element> {
+        AnyAsyncSequence(self)
+    }
+}
---
+++ Tests/AnyAsyncSequenceTests/AnyAsyncSequenceTests.swift
@@ -0,0 +1,7 @@
+import XCTest
+@testable import AnyAsyncSequence
+
+final class AnyAsyncSequenceTests: XCTestCase {
+    func testExample() throws {
+    }
+}
